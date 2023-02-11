012fe37d248830b6328904ed03989bae9977070a
utsav <guptautsavkumar126@gmail.com>
2023-02-11 17:58:54 +0530

    second

--- src/main/java/com/driver/OrderService.java
+++ src/main/java/com/driver/OrderService.java
@@ -7,8 +7,8 @@ import java.util.List;
 
 @Service
 public class OrderService {
-    @Autowired
-    OrderRepository orderRepository;
+
+    OrderRepository orderRepository=new OrderRepository();
 
     public String addOrder(Order order) {
         return orderRepository.addOrder(order);
